fe09a91b85d0162ee21494c4833d14d65984e281
Alberto Garibay <garib112@mail.chapman.edu>
2019-04-23 11:34:04 -0700

    Creating website and adding login

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'ramir266:torNado911!@jdbc:mysql://35.233.130.214:3306/maxxhaul' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
